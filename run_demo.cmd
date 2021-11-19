6aea39662d3e0ad0e12329d481a841c8592d53a4
Giorgio Silvestris <giorgio.silvestris@gmail.com>
2021-11-19 12:05:29 +0100

    update

--- run_demo.cmd
+++ run_demo.cmd
@@ -1,1 +1,15 @@
-"%JAVA_HOME%"\bin\java -cp ./target/swingup-1.0.0.jar;./lib/swing3rd.jar -Djsenv=demo org.dew.swingup.main.Main
+@echo off
+
+if "%JAVA_HOME%"=="" goto NOJAVAHOME
+
+set _CP=./target/swingup-1.0.0.jar;./lib/swing3rd.jar
+
+"%JAVA_HOME%"\bin\java -cp %_CP% -Djsenv=demo org.dew.swingup.main.Main
+
+goto DONE
+
+:NOJAVAHOME
+echo JAVA_HOME not setted.
+goto DONE
+
+:DONE
--- run_demo.sh
+++ run_demo.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-"$JAVA_HOME"/bin/java -cp ./target/swingup-1.0.0.jar:./lib/swing3rd.jar -Djsenv=demo org.dew.swingup.main.Main
+CP="./target/swingup-1.0.0.jar:./lib/swing3rd.jar"
+
+if [ "x$JAVA_HOME" = "x" ]; then
+  echo "JAVA_HOME not setted"
+else
+  "$JAVA_HOME"/bin/java -cp "$CP" -Djsenv=demo org.dew.swingup.main.Main
+fi
+
